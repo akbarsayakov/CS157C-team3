fe684aac58bf3247668e559ccb26439c00beab13
Anushree <anushreeshah24@gmail.com>
2023-05-08 13:34:14 -0700

    Changes in the Controller responses

--- backend/recipe-finder/src/main/java/edu/sjsu/recipefinder/controller/RecipeController.java
+++ backend/recipe-finder/src/main/java/edu/sjsu/recipefinder/controller/RecipeController.java
@@ -1,7 +1,7 @@
 package edu.sjsu.recipefinder.controller;
 
 import edu.sjsu.recipefinder.model.Message;
-import edu.sjsu.recipefinder.model.Recipe;
+import edu.sjsu.recipefinder.model.PostRecipe;
 import edu.sjsu.recipefinder.util.Constants;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,27 +10,27 @@ import org.springframework.web.bind.annotation.*;
 public class RecipeController {
 
     @GetMapping("/search")
-    public ResponseEntity<Recipe[]> recipe(@RequestParam Recipe recipe) {
+    public ResponseEntity<PostRecipe[]> recipe(@RequestParam PostRecipe recipe) {
         // TODO query saved recipes in redis and load matches into memory
         return null;
     }
 
 
     @PostMapping("/create")
-    public ResponseEntity<Message> create(@RequestParam Recipe recipe) {
+    public ResponseEntity<Message> create(@RequestParam PostRecipe recipe) {
         // TODO save this recipe in redis if not already present; handle errors
-        return ResponseEntity.ok(new Message(Constants.TEXT_WIP));
+        return ResponseEntity.ok(new Message());
     }
 
     @PutMapping("/edit")
-    public ResponseEntity<Message> edit(@RequestParam Recipe recipe) {
+    public ResponseEntity<Message> edit(@RequestParam PostRecipe recipe) {
         // TODO edit this recipe in redis if not absent; handle errors
-        return ResponseEntity.ok(new Message(Constants.TEXT_WIP));
+        return ResponseEntity.ok(new Message());
     }
 
     @PostMapping("/favorite")
-    public ResponseEntity<Message> favorite(@RequestParam Recipe recipe) {
+    public ResponseEntity<Message> favorite(@RequestParam PostRecipe recipe) {
         // TODO mark this recipe as a favorite in redis if not absent; handle errors
-        return ResponseEntity.ok(new Message(Constants.TEXT_WIP));
+        return ResponseEntity.ok(new Message());
     }
 }
--- backend/recipe-finder/src/main/java/edu/sjsu/recipefinder/util/Constants.java
+++ backend/recipe-finder/src/main/java/edu/sjsu/recipefinder/util/Constants.java
@@ -2,4 +2,8 @@ package edu.sjsu.recipefinder.util;
 
 public class Constants {
     public static String TEXT_WIP = "Work in progress!";
+    public static String WRONG_PASSWORD = "Bad Credentials. Cannot login at this moment.";
+    public static String NO_SUCH_USER = "User account does not exist. Cannot login at this moment.";
+
+    public static String USER_ALREADY_EXISTS = "Cannot create account. User with this email already exists.";
 }
